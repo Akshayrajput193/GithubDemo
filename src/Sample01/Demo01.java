f45b6183bd8f393bc8fd7ceac7f1359523cf9b97
devendra <devendra@LAPTOP-Q8SUV6JJ>
2021-04-05 18:21:08 +0530

    [akshay] adding the fresh changes to project

---
+++ src/Sample01/Demo01.java
@@ -0,0 +1,17 @@
+package Sample01;
+
+public class Demo01 {
+
+	 public static void main(String[] args) {
+		 
+		 System.out.println("adding new project");
+		 
+		 System.out.println("old one is deleted");
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+}
